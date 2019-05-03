93b91e6f1f7b4d5bcf67b8cd65dae5c8a75ee73e
Ian Walter <public@iankwalter.com>
2019-05-03 13:32:07 -0400

    Adding webdriver config

--- tests.js
+++ tests.js
@@ -3,9 +3,21 @@ const createTestServer = require('@ianwalter/test-server')
 const { start, stop } = require('.')
 
 test('tunneling localhost', async ({ browser, expect }) => {
-  const local = await start()
-  expect(local.pid).toBeGreaterThan(0)
   const server = await createTestServer()
+  server.use(ctx => {
+    ctx.body = `
+      <html>
+        <head>
+          <title>Hello World!</title>
+        </head>
+        <body>
+          <h1>Hello World!</h1>
+        </body>
+      </html>
+    `
+  })
+  await start()
   await browser.url(server.url)
+  expect(await browser.getTitle()).toBe('Hello World!')
   await stop()
 })
